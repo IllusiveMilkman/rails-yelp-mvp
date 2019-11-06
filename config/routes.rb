64171dd5d069df4e40110fed231cb4167aa774b8
Willem Booysen <whbooysen@gmail.com>
2019-11-06 15:31:09 +0100

    basic crud added for user:visitor

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:index, :show, :new, :create]
   end
+
+  # namespace :admin do
+  #   resources :restaurants, only: [:edit, :update, :destroy]
+  # end
 end
